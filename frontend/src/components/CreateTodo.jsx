966feb95c56493d4bc6a5db4ac6d76306ff5c5a3
Rajdeep Rudra <rajdeeprudra2003@gmail.com>
2025-06-26 04:10:59 +0530

    added the Todos section

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,7 +1,13 @@
 export function CreateTodo(){
     return <div>
-        <input type="text" placeholder="ttile" />
-        <input type="text" placeholder="description" />
+        <input style={{
+            padding: 10,
+            margin: 10
+        }} type="text" placeholder="title" /> <br />
+        <input style={{
+            padding: 10,
+            margin: 10
+        }} type="text" placeholder="description" /> <br />
         <button>Add a todo</button>
     </div>
 }
